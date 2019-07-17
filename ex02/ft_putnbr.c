02b57d00fb415047e721111cc9fd38ca2bf03b48
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-17 11:11:15 +0200

    ft_putnbr

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/17 10:56:43 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/17 11:09:38 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int n;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		n = nb * -1;
+	}
+	else
+	{
+		n = nb;
+	}
+	if (n > 0)
+	{
+		ft_putnbr(n / 10);
+		ft_putchar((n % 10) + '0');
+	}
+}
+
+int		main(void)
+{
+	ft_putnbr(-387);
+	return (0);
+}
