a843a8a1ec407a30000b32f9405cc9b91c3d874f
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-18 15:41:39 +0200

    not final putnbr base

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,11 +6,40 @@
 /*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 02:03:30 by ljongman          #+#    #+#             */
-/*   Updated: 2019/07/18 02:06:54 by ljongman         ###   ########.fr       */
+/*   Updated: 2019/07/18 15:41:10 by ljongman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+int		ft_countbase(char *base)
+{
+	unsigned int	nbase;
+	char			rep;
+	int				i;
+
+	nbase = 0;
+	while (base[nbase] != '\0')
+	{
+		if ((base[nbase] != '+') && (base[nbase] != '-')
+		&& (base[nbase] != 127) && ((base[nbase] < 0) || (base[nbase] > 32)))
+		{
+			rep = base[nbase];
+			nbase++;
+			i = nbase;
+			while (base[i] != '\0')
+			{
+				if (rep == base[i])
+					i++;
+				else
+					return (0);
+			}
+		}
+		else
+			return (0);
+	}
+	return (nbase);
+}
 
 void	ft_putchar(char c)
 {
@@ -20,37 +49,29 @@ void	ft_putchar(char c)
 void	ft_putnbr_base(int nbr, char *base)
 {
 	unsigned int	n;
-	int				nbase;
-	int				i;
+	unsigned int	nbase;
 
-	nbase = 0;
-	i = 0;
-	while (base[i] != '\0')
+	nbase = ft_countbase(base);
+	if (nbase > 1)
 	{
-		nbase++;
-		i++;
+		if (nbr < 0)
+		{
+			ft_putchar('-');
+			n = nbr * -1;
+		}
+		else
+			n = nbr;
+		if (n > nbase - 1)
+			ft_putnbr_base((n / nbase), base);
+		ft_putchar(base[n % nbase]);
 	}
-	if (nbr < 0)
-	{
-		ft_putchar('-');
-		n = nbr * -1;
-	}
-	else
-		n = nbr;
-	if (n > nbase - 1)
-	{
-		ft_putnbr_base((n / nbase), base);
-		ft_putchar((base[n % nbase]));
-	}
-	else
-		ft_putchar((base[n % nbase]));
 }
 
 int		main(void)
 {
 	int		nbr;
-	char	base[] = "0123456789abcdef";
+	char	*base = "01";
 
-	nbr = 639;
+	nbr = -6339;
 	ft_putnbr_base(nbr, base);
 }
